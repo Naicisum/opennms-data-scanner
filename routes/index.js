69994bb473bb69f01134cc3babe052fd3601fab6
Naicisum <Naicisum@users.noreply.github.com>
2022-07-11 23:49:47 -0500

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+/*eslint no-unused-vars: ["error", { "argsIgnorePattern": "next" }]*/
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
